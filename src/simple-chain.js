843555a43bcedbe3376d69443b21b0a668372d7a
Maria <pashkovichma@gmail.com>
2022-05-01 01:24:48 +0400

    feat: chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position <= 0 || position > this.getLength() || typeof(position) != 'number'){
+      throw new Error ('You can\'t remove incorrect link!');
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.map((el) =>`( ${el} )~~`).join('').slice(0, -2);
+    this.chain = [];
+    return result;
   }
 };
 
